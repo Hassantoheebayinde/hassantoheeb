033fc4570f6804be83cfc8295e2f0937c7390822
hassan <hassantoheebayinde@yahoo.com>
2020-03-24 22:37:42 +0100

    this is a javasript prograam which is tested using the index.html

---
+++ main.js
@@ -0,0 +1,10 @@
+var fullName = "Hassan Toheeb";
+var courses = ["go","php","python","c#","nodejs","javascript","ui/ux"];
+
+//Display of fuulname and courses
+console.log(fullName);
+console.log(courses);
+//courses offered == odd
+for(var i=1; i<=200; i+=2){
+    console.log(i);
+}
