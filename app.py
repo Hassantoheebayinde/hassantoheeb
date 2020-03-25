5e0cff71b4ea052724d15c55d17b9a62db4b8709
hassan <hassantoheebayinde@yahoo.com>
2020-03-25 18:43:51 +0100

    This is a python program that calculate the area of a circle based on the radius of the circle the user inputed

---
+++ app.py
@@ -0,0 +1,5 @@
+# A program that calculate the area of a circle using the user input
+radius = int(input("Please enter the radius of the circle you want to calculate its area: "))
+pi_value = 3.142
+area_of_circle = (radius*radius) * pi_value
+print(str(area_of_circle) + "cm^2")
